3ab6fc91baaf93291adf75ca7289b49556e5ed37
ughurba <ughurba@edu.code.az>
2022-06-05 21:29:29 +0400

    added new animation, added section discounts

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,16 +1,16 @@
-new Swiper(".mySwiper", {
+new Swiper(".mySwiper",{
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
       dynamicBullets: true,
     },
-    // loop: true,
-    // grabCursor:true,
-    // autoplay: {
-    //   delay: 2000,
-    //   stopOnLastSlide: false,
-    //   disableOnInteraction: false,
-    // },
+    loop: true,
+    grabCursor:true,
+    autoplay: {
+      delay: 2000,
+      stopOnLastSlide: false,
+      disableOnInteraction: false,
+    },
     speed: 1000,
     navigation:{
         nextEl:'.swiper-button-next',
@@ -20,6 +20,42 @@ new Swiper(".mySwiper", {
    
   });
 
+
+
+
+
+
+
+
+
+
+  new Swiper( '.swiper-container',{
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    centeredSlides:	true,
+    
+    loop: true,
+   grabCursor:true,
+  //  slidesPerView: 1,
+  //   slidesPerGroup: 1,
+    speed: 1000,
+    navigation:{
+      
+        nextEl:'.swiper-button-next',
+        prevEl:'.swiper-button-prev',
+    },
+    
+
+    
+  
+   
+   })
+
+
+ 
 
 
 
