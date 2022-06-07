e2b6af696db149ffb8b2cb0c32ea105d0ae9ae8d
ughurba <ughurba@edu.code.az>
2022-06-07 15:34:08 +0400

    fix-bug main section

--- assets/js/product-detail.js
+++ assets/js/product-detail.js
@@ -7,8 +7,6 @@ const subCategories = document.querySelector(".dropdown-categories");
 
 
 
-
-
 function modalWindowClassAddHandler() {
     modalWindow.classList.remove("hidden");
     overlay.classList.remove("hidden");
@@ -30,3 +28,73 @@ function modalWindowClassAddHandler() {
   closeBtn.addEventListener("click", modalWindowClassRemoveHandler);
   selectLocation.addEventListener("click", modalWindowClassAddHandler);
   btnCategories.addEventListener("click", categoriesWindowRemoveHandler);
+
+
+
+
+  //------------------------------////
+  // main-slide-img
+
+
+  const mainImgs = document.querySelectorAll('.main-img')
+  const slideImgs = document.querySelectorAll('.child-img')
+
+  slideImgs.forEach(imgChild =>{
+
+
+    
+      imgChild.addEventListener('click', showsHandler);
+  
+
+        function showsHandler(){
+          const dataIdImg =  imgChild.getAttribute('data-id');
+      
+          mainImgs.forEach(mainImg =>{
+      
+            const dataIdMainImg =  mainImg.getAttribute('data-id')
+                      if (dataIdImg === dataIdMainImg){                    
+                        mainImg.classList.remove('hidden')
+                        imgChild.parentElement.classList.add('slide-img-border')
+                       
+                          
+                      }else if(dataIdImg !== dataIdMainImg){
+                        mainImg.classList.add('hidden')
+                                      
+                      } 
+                      
+          })
+        }
+      
+    
+    
+  })
+
+
+  // count-product -----------------------------//
+
+const minus = document.querySelector('.minus');
+const plus = document.querySelector('.plus');
+const count = document.querySelector('.count');
+
+
+
+let num =1;
+function minusHandler(){
+ 
+
+  if(num >= 2){
+    num--;
+    count.textContent = num
+  }
+  
+
+}
+
+function plusHandler(){
+  num++ 
+  count.textContent = num
+}
+
+
+minus.addEventListener('click',minusHandler)
+plus.addEventListener('click',plusHandler)
