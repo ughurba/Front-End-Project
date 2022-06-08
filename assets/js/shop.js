ed12ec86ee08de202beda1ffe73e2faf024d6057
ughurba <ughurba@edu.code.az>
2022-06-09 03:40:04 +0400

    wrote the functionality for the categories

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -27,7 +27,57 @@ closeBtn.addEventListener("click", modalWindowClassRemoveHandler);
 selectLocation.addEventListener("click", modalWindowClassAddHandler);
 btnCategories.addEventListener("click", categoriesWindowRemoveHandler);
 
+//categories
 
+const plusBtn = document.querySelectorAll(".fa-plus");
+const subMenu = document.querySelectorAll(".sub-menu");
+const minusBtn = document.querySelectorAll(".bi-dash");
+plusBtn.forEach((plus) => {
+  function showProductCategoriesHandler() {
+    const plusId = plus.getAttribute("data-id");
 
+    subMenu.forEach((menu) => {
+      const menuId = menu.getAttribute("data-id");
 
-//------------------FILTER-----------------------
+      if (menuId == plusId) {
+        styleAndClasslist(menu, "hidden-categories", "remove", plus, "none");
+
+        minusBtn.forEach((minus) => {
+          const minusId = minus.getAttribute("data-id");
+          if (minusId == plusId) {
+            styleAndClasslist(minus, "hidden", "remove");
+          }
+
+          function minusHiddenHandler() {
+            if (minusId == menuId) {
+              styleAndClasslist(minus, "hidden", "add", plus, "block");
+              styleAndClasslist(menu, "hidden-categories", "add");
+            }
+          }
+
+          minus.addEventListener("click", minusHiddenHandler);
+        });
+      }
+    });
+  }
+
+  plus.addEventListener("click", showProductCategoriesHandler);
+});
+
+const styleAndClasslist = (
+  item,
+  className,
+  propClasslist,
+  displayItem,
+  display
+) => {
+  if (displayItem !== undefined || display !== undefined) {
+    displayItem.style.display = display;
+  }
+
+  if (propClasslist === "add") {
+    item.classList.add(className);
+  } else if (propClasslist === "remove") {
+    item.classList.remove(className);
+  }
+};
