243dc4e99f4f7edcf411a65977dbba479ace2aa3
ughurba <ughurba@edu.code.az>
2022-06-10 18:26:15 +0400

    I started basket

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -82,72 +82,147 @@ const styleAndClasslist = (
   }
 };
 
-
 //goods number ////////////////////
 
+const goods = document.querySelectorAll(".goods-product");
+const numbers = document.querySelectorAll(".numbers");
+const leftArrow = document.querySelector(".bi-arrow-left");
 
+goods.forEach((product) => {
+  const productId = product.getAttribute("data-id");
 
-const goods = document.querySelectorAll('.goods-product')
-const numbers = document.querySelectorAll('.numbers')
-const leftArrow = document.querySelector('.bi-arrow-left')
+  numbers.forEach((number) => {
+    const numberId = number.getAttribute("data-id");
 
-goods.forEach(product =>{
+    function jumpByNumbers() {
+      numberId == 1
+        ? (leftArrow.style.display = "none")
+        : (leftArrow.style.display = "block");
 
- const productId =  product.getAttribute('data-id');
- 
+      if (!product.classList.contains("hidden-goods")) {
+        styleAndClasslist(product, "hidden-goods", "add");
+      }
 
- numbers.forEach(number =>{
-   const numberId = number.getAttribute('data-id')
+      numbers.forEach((num) => {
+        if (num.classList.contains("number-background")) {
+          styleAndClasslist(num, "number-background", "remove");
+        }
+      });
 
- 
+      if (productId == numberId) {
+        styleAndClasslist(product, "hidden-goods", "remove");
+      }
 
-
-   function jumpByNumbers(){
-
-    numberId == 1 ?  leftArrow.style.display='none' : leftArrow.style.display='block'
-    
- 
-
-    if(!product.classList.contains('hidden-goods')){
-     
-      styleAndClasslist(product,'hidden-goods','add');
-
+      styleAndClasslist(number, "number-background", "add");
     }
 
-numbers.forEach(num=>{
-  if(num.classList.contains('number-background')){
-   
-    styleAndClasslist(num,'number-background','remove');
+    number.addEventListener("click", jumpByNumbers);
+  });
+});
+
+//-------------------basket-------------------------------------//
+
+
+
+
+
+
+
+
+
+const btnAddtoCard = document.querySelectorAll(".button-goods");
+const totalBasket = document.querySelector('.price-basket');
+const countBasket = document.querySelector('.count-basket')
+
+
+
+
+
+
+
+
+function addingItemToCartHandler(ev) {
+  ev.preventDefault();
+
+  if (localStorage.getItem("basket") == null) {
+    localStorage.setItem("basket", JSON.stringify([]));
   }
-})
 
  
 
-    if(productId == numberId){
-    
-      styleAndClasslist(product,'hidden-goods','remove');
-    }
-   
-   
-   
-    styleAndClasslist(number,'number-background','add');
-   
+  let productId = this.parentElement.parentElement.getAttribute("data-id");
+  let goodsList = JSON.parse(localStorage.getItem("basket"));
 
+ 
+  if (!renderBasketCount(productId)) {
 
+    goodsList.push({
+      id: productId,
+      img: this.parentElement.parentElement.firstElementChild.firstElementChild.nextElementSibling.nextElementSibling.getAttribute(
+        "src"
+      ),
+      title:
+        this.parentElement.parentElement.firstElementChild.nextElementSibling
+          .firstElementChild.textContent,
+      price:
+        this.parentElement.parentElement.lastElementChild.previousElementSibling
+          .lastElementChild.firstElementChild.textContent,
+      count: 1,
+    });
+
+     localStorage.setItem("basket", JSON.stringify(goodsList));
+     sizeBasket()
   }
+
+
+}
+
+
+
+
+function renderBasketCount(productId) {
+
+  let goodsList = JSON.parse(localStorage.getItem("basket"));
+
+  let existProduct = goodsList.find((p) => p.id == productId);
 
   
+  if(existProduct !== undefined){
+
+    existProduct.count++;
+   
+     localStorage.setItem("basket", JSON.stringify(goodsList));
+    
+    return true;
+  }
+
+
+}
 
 
 
-   number.addEventListener('click',jumpByNumbers);
- })
+const  sizeBasket = () => {
+  let goodsList = JSON.parse(localStorage.getItem("basket"));
+  countBasket.textContent = goodsList.length
+}
 
 
-})
 
 
-//-------------------FILTER-------------------------------------//
 
-// const filterInput = document.querySelector('.form-range')
-// console.log(filterInput.value);
+
+
+btnAddtoCard.forEach((btn) => {
+  btn.addEventListener("click", addingItemToCartHandler);
+});
+
+sizeBasket();
+
+
+
+
+
+
+// 6. removeFromBasket(id) - localestaregeden arrayi goturur parse eliyir, gelen id-ni silir. yeniden local stargeye qoyur (addin icinde islenen funksiyalar burda da isleyir)
+
+// 7. renderBasketGoods(goods from localStorage) - localstoragedeki arrayi goturur parse edir. ordaki id-lere uygun goods arrayinden mallari alir render edir
